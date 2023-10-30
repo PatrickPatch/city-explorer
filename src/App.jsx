67b5a9caa4069596a0c4ef8cecd87d0dde4db431
PatrickPatch <patrickclarke1983@gmail.com>
2023-10-30 20:49:39 +0000

    finished first draft of app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,37 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import axios from "axios";
 import "./App.css";
 
-function App() {}
+const API_KEY = import.meta.env.VITE_API_KEY;
+
+function App() {
+  const [location, setLocation] = useState({});
+  const [search, setSearch] = useState("");
+
+  function handleChange(event) {
+    setSearch(event.target.value);
+  }
+  async function getLocation(event) {
+    event.preventDefault();
+
+    const API =
+      "httlps://eu1.locationiq.com/v1/search?q${search}&key=${API_KEY}&format=json";
+
+    const res = await axios.get(API);
+
+    setLocation(res.data[0]);
+  }
+  return (
+    <>
+      <h1>APIs</h1>
+      <form onSubmit={getLocation}>
+        <input onChange={handleChange} placeholder="Location" />
+        <button>Get Location</button>
+      </form>
+
+      <h2>{location.display_name}</h2>
+    </>
+  );
+}
 
 export default App;
